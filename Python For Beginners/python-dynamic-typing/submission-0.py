18e86e7b5d21ea858adf285bb05709b58827bd95
Brady Royer <royerbraedan@gmail.com>
2026-08-09 10:56:50 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 10.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'Hello There'
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
